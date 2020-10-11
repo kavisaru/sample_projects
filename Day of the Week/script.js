ca92ca3f16e8567e6e7fef6a1cf50b36a020f2b8
kavisaru <64577758+kavisaru@users.noreply.github.com>
2020-10-11 07:52:19 +0000

    Add files via upload

---
+++ Day of the Week/script.js
@@ -0,0 +1,39 @@
+let date=new Date();
+let dayOfWeekNumber = date.getDay();
+let nameOfDay;
+let quote;
+switch(dayOfWeekNumber){
+ case 0: 
+    nameOfDay = 'Sunday';
+    quote = 'chillax!';
+    break;
+  case 1:
+    nameOfDay = 'Monday';
+    quote = 'Boring!';
+    break;
+  case 2:
+    nameOfDay = 'Tuesday';
+    quote = 'Taco Time!';
+    break;
+  case 3:
+    nameOfDay = 'Wednesday';
+    quote = 'Two more days to the weekend.';
+    break;
+  case 4:
+    nameOfDay = 'Thursday';
+    quote = 'The weekend is almost here...';
+    break;
+  case 5:
+    nameOfDay = 'Friday';
+    quote = 'Weekend is here!';
+     break;
+  case 6:
+    nameOfDay = 'Saturday';
+    quote = 'Time to party!';
+    break;
+  
+}
+let weekdayDiv = document.getElementById('weekday');
+weekdayDiv.innerHTML = `${nameOfDay}`;
+let quoteDiv = document.getElementById('phrase');
+quoteDiv.innerHTML = `${quote}`;
